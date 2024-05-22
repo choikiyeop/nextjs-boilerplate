ecd51fde7ffc158c594d6c25840c8ebf124da8a0
choikiyeop <cky9709@naver.com>
2024-05-22 14:33:45 +0900

    move font config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { inter } from "@/libs/fonts";
+import { inter } from "@/constants/configs/fonts";
 
 export const metadata: Metadata = {
   title: "Nextjs Boilerplate",
