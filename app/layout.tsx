55802d9ee0765a9696907970651b41ddcb87fccf
choikiyeop <cky9709@naver.com>
2024-05-28 12:50:20 +0900

    feat: add shadcn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { fontSans } from "@/constants/configs/fonts";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Nextjs Boilerplate",
@@ -22,6 +23,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
