f100bcea0a07330722dffe901edb67c88c9342e7
choikiyeop <cky9709@naver.com>
2024-05-30 17:25:40 +0900

    feat: post card 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { PostCard } from "@/components/elements/post-card";
 import { PageLayout } from "@/components/layouts/page-layout";
 import {
   Carousel,
@@ -6,7 +7,67 @@ import {
 } from "@/components/ui/carousel";
 import Image from "next/image";
 
-export default function Home() {
+const posts = [
+  {
+    id: 1,
+    datetime: new Date().toString(),
+    date: "Mar 16, 2020",
+    category: {
+      href: "#",
+      title: "Marketing",
+    },
+    href: "#",
+    title: "Boost your conversion rate",
+    description:
+      "Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta",
+    author: {
+      imageUrl: "",
+      href: "#",
+      name: "Michael Foster",
+      role: "Front-end Developer",
+    },
+  },
+  {
+    id: 2,
+    datetime: new Date().toString(),
+    date: "Mar 16, 2020",
+    category: {
+      href: "#",
+      title: "Marketing",
+    },
+    href: "#",
+    title: "Boost your conversion rate",
+    description:
+      "Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta",
+    author: {
+      imageUrl: "",
+      href: "#",
+      name: "Michael Foster",
+      role: "Front-end Developer",
+    },
+  },
+  {
+    id: 3,
+    datetime: new Date().toString(),
+    date: "Mar 16, 2020",
+    category: {
+      href: "#",
+      title: "Marketing",
+    },
+    href: "#",
+    title: "Boost your conversion rate",
+    description:
+      "Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta",
+    author: {
+      imageUrl: "",
+      href: "#",
+      name: "Michael Foster",
+      role: "Front-end Developer",
+    },
+  },
+];
+
+export default async function Home() {
   return (
     <PageLayout>
       <Carousel opts={{ loop: true }}>
@@ -22,7 +83,13 @@ export default function Home() {
           </CarouselItem>
         </CarouselContent>
       </Carousel>
-      <main>페이지</main>
+      <main className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto my-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
+      </main>
     </PageLayout>
   );
 }
